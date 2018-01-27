9697785cb8aca73b5ae0773f944d0e72a7dde135
davidtron <david@spiraltechnology.co.uk>
2018-01-27 15:18:34 +0000

    List and view notes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -4,8 +4,10 @@ import AppliedRoute from "./components/AppliedRoute";
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import Signup from "./containers/Signup";
-import NotFound from "./containers/NotFound";
 import NewNote from "./containers/NewNote";
+import Notes from "./containers/Notes";
+import NotFound from "./containers/NotFound";
+
 
 
 export default ({childProps}) =>
@@ -14,6 +16,7 @@ export default ({childProps}) =>
         <AppliedRoute path="/signup" exact component={Signup} props={childProps} />
         <AppliedRoute path="/login" exact component={Login} props={childProps}/>
         <AppliedRoute path="/notes/new" exact component={NewNote} props={childProps} />
+        <AppliedRoute path="/notes/:id" exact component={Notes} props={childProps} />
 
         {/* Finally, catch all unmatched routes */}
         <Route component={NotFound}/>
